149b6cebcf18fcaddec492e51d650e72d70f431f
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2016-12-11 13:59:26 -0800

    Multiband compressor synthdef uses DB inputs for amplitude.

--- supriya/synthdefs/multiband_compressor.py
+++ supriya/synthdefs/multiband_compressor.py
@@ -5,9 +5,9 @@ from supriya import ugentools
 
 def _make_synthdef(channel_count=2):
     with SynthDefBuilder(
-        frequency_1=100,
-        frequency_2=1000,
-        frequency_3=3000,
+        frequency_1=200,
+        frequency_2=2000,
+        frequency_3=5000,
         band_1_clamp_time=0.01,
         band_1_postgain=1,
         band_1_pregain=1,
@@ -65,37 +65,37 @@ def _make_synthdef(channel_count=2):
             relax_time=builder['band_1_relax_time'],
             slope_above=builder['band_1_slope_above'],
             slope_below=builder['band_1_slope_below'],
-            source=band_1 * builder['band_1_pregain'],
-            threshold=builder['band_1_threshold'],
+            source=band_1 * builder['band_1_pregain'].db_to_amplitude(),
+            threshold=builder['band_1_threshold'].db_to_amplitude(),
             )
         band_2 = ugentools.CompanderD.ar(
             clamp_time=builder['band_2_clamp_time'],
             relax_time=builder['band_2_relax_time'],
             slope_above=builder['band_2_slope_above'],
             slope_below=builder['band_2_slope_below'],
-            source=band_2 * builder['band_2_pregain'],
-            threshold=builder['band_2_threshold'],
+            source=band_2 * builder['band_2_pregain'].db_to_amplitude(),
+            threshold=builder['band_2_threshold'].db_to_amplitude(),
             )
         band_3 = ugentools.CompanderD.ar(
             clamp_time=builder['band_3_clamp_time'],
             relax_time=builder['band_3_relax_time'],
             slope_above=builder['band_3_slope_above'],
             slope_below=builder['band_3_slope_below'],
-            source=band_3 * builder['band_3_pregain'],
-            threshold=builder['band_3_threshold'],
+            source=band_3 * builder['band_3_pregain'].db_to_amplitude(),
+            threshold=builder['band_3_threshold'].db_to_amplitude(),
             )
         band_4 = ugentools.CompanderD.ar(
             clamp_time=builder['band_4_clamp_time'],
             relax_time=builder['band_4_relax_time'],
             slope_above=builder['band_4_slope_above'],
             slope_below=builder['band_4_slope_below'],
-            source=band_4 * builder['band_4_pregain'],
-            threshold=builder['band_4_threshold'],
+            source=band_4 * builder['band_4_pregain'].db_to_amplitude(),
+            threshold=builder['band_4_threshold'].db_to_amplitude(),
             )
-        band_1 *= builder['band_1_postgain']
-        band_2 *= builder['band_2_postgain']
-        band_3 *= builder['band_3_postgain']
-        band_4 *= builder['band_4_postgain']
+        band_1 *= builder['band_1_postgain'].db_to_amplitude()
+        band_2 *= builder['band_2_postgain'].db_to_amplitude()
+        band_3 *= builder['band_3_postgain'].db_to_amplitude()
+        band_4 *= builder['band_4_postgain'].db_to_amplitude()
         source = ugentools.Sum4.new(
             input_one=band_1,
             input_two=band_2,
